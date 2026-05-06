4cbb855f2c88153723ec7921aeff0bc412244c0f
malhunhatun1971 <malhunhatun1971@gmail.com>
2026-05-06 14:22:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ def keep_alive():
     t.start()
 
 # ----------- ২. কনফিগারেশন -----------
-BOT_TOKEN = "8673693249:AAE_Y8S-jHT9S5IYjN_nX4U8o-c2nZNRpbc"
+BOT_TOKEN = "8673693249:AAHaXWtETW1jmE8-jDD_9PkZ-E3L-fCgM2k"
 
 # সেশন মেইনটেইন করার জন্য
 session = requests.Session()
